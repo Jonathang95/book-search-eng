3b01ee8ba967f06707c55cfea3f703eb0ad8282a
unknown <jongarcia.realty@gmail.com>
2024-06-21 14:55:32 -0700

    added to schemas and utils

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,71 @@
+const { Query } = require('mongoose');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (_,_, context) => {
+            if (context.user) {
+            const foundUser = await User.findOne({_id: context.user._id});
+            return foundUser;
+            }
+            throw new Error('You need to be logged in!');
+        },
+    },
+
+    Mutation: {
+        login: async (_, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new Error('No user found with this email!');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if (!correctPw) {
+                throw new Error('Incorrect password!');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        addUser: async (_, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        saveBook: async (_, { bookInput }, context) => {
+            if (!context.user) {
+              throw new Error('You need to be logged in!');
+            }
+      
+            try {
+              const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: bookInput } },
+                { new: true, runValidators: true }
+              );
+              return updatedUser;
+            } catch (err) {
+              console.log(err);
+              throw new Error('Failed to save the book');
+            }
+          },
+      
+          removeBook: async (_, { bookId }, context) => {
+            if (!context.user) {
+              throw new Error('You need to be logged in!');
+            }
+      
+            const updatedUser = await User.findOneAndUpdate(
+              { _id: context.user._id },
+              { $pull: { savedBooks: { bookId } } },
+              { new: true }
+            );
+      
+            if (!updatedUser) {
+              throw new Error("Couldn't find user with this id!");
+            }
+      
+            return updatedUser;
+        },
+    },
+};    
--- server/server.js
+++ server/server.js
@@ -2,6 +2,10 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
+const { ApolloSever } = require('apollo-sever-express')
+const { typeDefs, resolvers } = require('./schema')
+const { authMiddleware } = require('./utils/auth');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -16,6 +20,19 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+
+server.start().then(res => {
+  server.applyMiddleware({ app })
+
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+  console.log(`🌍 Now listening on localhost:${PORT}`);
+  console.log(`GraphQL endpoint available at http://localhost:${PORT}${server.graphqlPath}`);
+    });
+  });
 });
